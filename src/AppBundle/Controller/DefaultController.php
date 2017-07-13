68bf11e09c501f0bed6399e1f5cbcd599636e614
thibmonier <thibaut.monier@gmail.com>
2017-07-13 19:01:15 +0200

    remove useless controller

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -19,14 +19,4 @@ class DefaultController extends Controller
         ]);
     }
 
-    /**
-     * @Route("/login", name="login")
-     */
-/*    public function loginAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/login.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
-    }*/
 }
